92a7cb2287feeac8b49a90547f15bffdbfacac77
Swaroopa-16 <swaroopa.aids24@cmrit.ac.in>
2025-11-25 19:20:48 +0530

    Update API_URL to new endpoint

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // JSONP + iframe-based POST approach (no CORS issues)
 // Apps Script endpoint (must match the form action)
-const API_URL = "https://script.google.com/macros/s/AKfycbzcZZaOv5aeHTC8t7Zn8ze9CsNJ1wn3CzcJrMn_n041oFlVX9wnNSR7VWDl0_0eDJ-keA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzclTSeEeMwdtFt9q0sgorfOk4RFTcpigRt7XCRNJU2EbMzLMxWKtHCoFYv77pwtk-BEQ/exec";
 
 
 function jsonpFetch(url) {

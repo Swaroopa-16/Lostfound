248ed8d19532d834bd54bc40c57760d0ce3faeec
Swaroopa-16 <swaroopa.aids24@cmrit.ac.in>
2025-12-07 16:37:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* ------------------ CONFIG - DO NOT FORGET ------------------ */
 // Your Apps Script /exec URL (you provided this)
-const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbzcZZaOv5aeHTC8t7Zn8ze9CsNJ1wn3CzcJrMn_n041oFlVX9wnNSR7VWDl0_0eDJ-keA/exec';
+const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbzclTSeEeMwdtFt9q0sgorfOk4RFTcpigRt7XCRNJU2EbMzLMxWKtHCoFYv77pwtk-BEQ/exec';
 
 // Logo path in repo root
 const LOGO_PATH = 'cmrit_logo.webp';
